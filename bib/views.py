90a2ba6b6fbe68d58197a25e1435dca8c0cc6211
Javier Lopez <jlopez@fi.upm.es>
2010-04-30 13:40:50 +0200

    Soporte para ordernar por tipo y año

--- bib/views.py
+++ bib/views.py
@@ -8,22 +8,41 @@ def group_by_year(publications):
         year['publications'] = publications.filter(year=year['year'])
     return years
 
+def group_by_type(publications):
+    types = publications.values('pub_type').distinct().order_by('pub_type')
+    for t in types:
+        all_pubs = publications.filter(pub_type=t['pub_type'])
+        t['years'] = group_by_year(all_pubs)
+    return types
+
+def process_response(publications, order_by):
+    if (order_by == 'type'): 
+        types = group_by_type(publications)
+        return render_to_response('publications.html',
+            {'list_template': 'by_type.html', 'types':  types})
+    elif (order_by == 'year'):
+        years = group_by_year(publications)
+        return render_to_response('publications.html', 
+            { 'list_template': 'by_year.html', 'type': {'years': years }}) 
+    else: 
+        raise Http404
+
 
 def view_all(request):
     publications = Publication.objects.filter(author__institute='Facultad de Informatica')
-    years = group_by_year(publications)
 
-    return render_to_response('publications.html', 
-        { 'list_template': 'by_year.html', 'years': years }) 
+    order_by = request.GET.get('order_by', 'year')
 
+    return process_response(publications, order_by)
 
 def view_author(request, author_id):
     author = get_object_or_404(Author, author_id=author_id, 
         institute='Facultad de Informatica')
-    years = group_by_year(author.publications)
 
-    return render_to_response('publications.html', 
-        { 'list_template': 'by_year.html', 'years': years }) 
+    order_by = request.GET.get('order_by', 'year')
+
+    return process_response(author.publications, order_by)
+
 
     #import ipdb; ipdb.set_trace()
 
