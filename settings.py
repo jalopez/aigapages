6afebdda82675cab4373460953cc160d305d815e
Javier Lopez <jlopez@fi.upm.es>
2010-05-03 17:33:36 +0200

    Primera version de la internacionalizacion

--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ TIME_ZONE = 'Europe/Spain'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 SITE_ID = 1
 
